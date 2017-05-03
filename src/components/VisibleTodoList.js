ef4ea6c814151dbe2a1dfbc28fa50665d316316a
Diego Cáceres <dado90caceres@gmail.com>
2017-05-03 14:31:26 -0300

    refactored Todos Reducer to be like a database

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,23 +2,12 @@ import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
+import { getVisibleTodos } from '../reducers';
 
 const mapStateToProps = (state, ownProps) => {
   return {
     todos: getVisibleTodos(
-      state.todos,
+      state,
       ownProps.filter
     ),
   };
